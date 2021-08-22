d900a420efd64c18afe520d322bae824b69f4510
Matheus Bonifácio <matheushgbonifacio@gmail.com>
2021-08-22 09:33:26 -0300

    add index.html

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,46 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+/* Mudar o HEADER da página quando der SCROLL*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do HEADER
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do HEADER
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonials Carousel Slider Swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* Scrollreveal - Mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
